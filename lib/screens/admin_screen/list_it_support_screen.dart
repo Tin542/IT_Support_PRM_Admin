d7b0744501b8fde03542fe4166796d77951b3b71
VuongLPH <VuongLPH1204@gmail.com>
2021-10-31 22:49:37 +0700

    bo nhung trang khong can thiet

--- lib/screens/admin_screen/list_it_support_screen.dart
+++ lib/screens/admin_screen/list_it_support_screen.dart
@@ -3,9 +3,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:it_support/screens/components/Add_IT_support.dart';
-
-import 'edit_screen.dart';
+import 'package:it_support/screens/auth_screen/register_screen_itsupport.dart';
 
 class ListITPage extends StatefulWidget {
   ListITPage({Key? key}) : super(key: key);
@@ -157,35 +155,6 @@ class _ListITPageState extends State<ListITPage> {
             children: [
               GestureDetector(
                 onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => Edit(
-                                contactKey: contact['key'],
-                              )));
-                },
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.edit,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(width: 6),
-                    Text(
-                      'Edit',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.blue,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              GestureDetector(
-                onTap: () {
                   _showDeleteDialog(contact: contact);
                 },
                 child: Row(
@@ -256,7 +225,7 @@ class _ListITPageState extends State<ListITPage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return AddIT();
+            return RegisterScreenIT();
           }));
         },
         child: Icon(
